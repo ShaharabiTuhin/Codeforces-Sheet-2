1b0e49ff2f6863e8a197cbbadc3f4c8341a2d68a
Shaharabi Tuhin <128482427+ShaharabiTuhin@users.noreply.github.com>
2023-12-09 20:59:30 +0600

    J.cpp

--- j2.c
+++ j2.c
@@ -1,15 +1,17 @@
-#include<stdio.h>
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
 int main(){
-    int i,n,f=0;
-    scanf("%d",&n);
-        for(i=2;i<n;i++)
-        {
-            if(n%i==0||n%i==1)
-            {
-                //printf("%d ",i);
-            }else{
-                printf("%d ",i);
-            }
+    int i,j,n,f=0;
+    cin >> n;
+    for(i=2;i<=n;i++){
+        f=0;
+        for(j=2;j<i;j++){
+            if(i%j==0)
+            f=1;
         }
-    return 0;
+    if(f==0)
+            cout << i << " ";
+    }
 }
+
